1ae06ee0770009106a1c0987bf20c825448f12cc
Abhijeet Singh <mrabhirajput5@gmail.com>
2025-12-16 11:34:50 +0530

    Remove nested Ecomplain folder - files should be at root level

--- Ecomplain/ecomplain-backend/src/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStudentDashboard } = require('../controllers/dashboardController');
-const { authenticateToken } = require('../middleware/auth');
-
-// Protected routes (require authentication)
-router.get('/', authenticateToken, getStudentDashboard);
-
-module.exports = router;
